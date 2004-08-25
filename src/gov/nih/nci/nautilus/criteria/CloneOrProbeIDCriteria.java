459f2b6135ca30c48cbc0acba5c6ff03411f3ead
Ram Bhattaru <bhattarr>
2004-08-25 22:15:17 +0000

    *** empty log message ***
    SVN-Revision: 66

---
+++ src/gov/nih/nci/nautilus/criteria/CloneOrProbeIDCriteria.java
@@ -0,0 +1,47 @@
+package gov.nih.nci.nautilus.criteria;
+
+import gov.nih.nci.nautilus.de.GeneIdentifierDE;
+import gov.nih.nci.nautilus.de.CloneIdentifierDE;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: BhattarR
+ * Date: Jul 12, 2004
+ * Time: 6:44:58 PM
+ * To change this template use Options | File Templates.
+ */
+public class CloneOrProbeIDCriteria extends Criteria {
+    private Collection cloneIdentifiers;
+    public Collection getCloneIdentifiers() {
+        return cloneIdentifiers;
+    }
+    public void setCloneIdentifiers(Collection cloneIdentifiersObjs) {
+        for (Iterator iterator = cloneIdentifiersObjs.iterator(); iterator.hasNext();) {
+            Object obj = iterator.next();
+            if (obj instanceof CloneIdentifierDE) {
+                getCloneIdentifiers().add(obj);
+            }
+        }
+    }
+
+    public void setGeneIdentifier(CloneIdentifierDE cloneIdentifier) {
+        getGeneIdentifiersMember().add(cloneIdentifier);
+    }
+    private Collection getGeneIdentifiersMember() {
+        if (cloneIdentifiers == null)
+            cloneIdentifiers  = new ArrayList();
+        return cloneIdentifiers ;
+    }
+
+    public CloneOrProbeIDCriteria() {
+    }
+
+    public boolean isValid() {
+        // TODO: see if we need any validation on GeneSymbols/LocusLinkID/GenBank etc
+        return true;
+    }
+}
