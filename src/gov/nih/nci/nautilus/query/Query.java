0a56e31a239ddcfa65ff1193f81d362e9097500a
Priti Shah <Priti.Shah@AoA.hhs.gov>
2004-09-07 17:08:22 +0000

    Added the isEmpty() method - pcs
    SVN-Revision: 90

--- src/gov/nih/nci/nautilus/query/Query.java
+++ src/gov/nih/nci/nautilus/query/Query.java
@@ -2,9 +2,14 @@ package gov.nih.nci.nautilus.query;
 
 import gov.nih.nci.nautilus.view.View;
 import gov.nih.nci.nautilus.queryprocessing.QueryHandler;
+import gov.nih.nci.nautilus.criteria.*;
 
 import java.util.Collection;
 import java.util.ArrayList;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,6 +23,43 @@ abstract public class Query {
     private String queryName;
     private View associatedView;
     public abstract QueryHandler getQueryHandler() throws Exception;
+    
+    //added by Prashant
+    public abstract QueryType getQueryType() throws Exception;
+    public abstract String toString();
+    
+	//TODO: The followig method checks if a given Query is empty
+	public boolean isEmpty() {
+		try {
+		
+			String currObjectName = this.getClass().getName();
+			Class currClass = Class.forName(currObjectName);
+			Method[] allPublicmethods = currClass.getMethods();
+				
+			for (int i = 0; i < allPublicmethods.length; i++) {
+				Method currMethod = allPublicmethods[i];
+				String currMethodString = currMethod.getName();
+				if ((currMethodString.toUpperCase().startsWith("GET")) 
+					&& (currMethod.getModifiers() == Modifier.PUBLIC))
+				{
+						Object thisObj = currMethod.invoke(this, null);
+	
+						if (thisObj != null) {
+							if (Criteria.class.isInstance(thisObj)) {
+								Criteria thisCriteria = (Criteria) thisObj;
+								if (!thisCriteria.isEmpty()) {return false;}
+							}
+						}
+				}
+			}
+				
+		 } catch (Throwable e) {
+				e.printStackTrace();
+		 }
+		 return true;
+	}
+
+    
 
     Query() {
        // criteriaCol = new ArrayList();
