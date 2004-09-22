f14a45465a581c2fb1a5f12f545f70e080cff51c
Himanso Sahni <Himanso.Sahni@fda.hhs.gov>
2004-09-22 22:34:25 +0000

    added testResultset
    SVN-Revision: 136

--- src/gov/nih/nci/nautilus/queryprocessing/ResultsetProcessor.java
+++ src/gov/nih/nci/nautilus/queryprocessing/ResultsetProcessor.java
@@ -17,6 +17,7 @@ import java.util.*;
 public class ResultsetProcessor {
 	public GeneViewContainer geneViewContainer = new GeneViewContainer();
 	public void createGeneView(Collection differentialExpressionSfact){
+		if(differentialExpressionSfact != null && differentialExpressionSfact.size()>0) {
 		Iterator iter = differentialExpressionSfact.iterator();
 	      while (iter.hasNext()){
 	      	DifferentialExpressionSfact diffExpSfact = (DifferentialExpressionSfact) iter.next();
@@ -28,31 +29,43 @@ public class ResultsetProcessor {
 	      			geneResultset = new GeneResultset();
 	      		}
 	      		GeneIdentifierDE.GeneSymbol geneSymbol = new GeneIdentifierDE.GeneSymbol(diffExpSfact.getGeneSymbol());
-//	      	    GeneIdentifierDE.GenBankAccessionNumber genbankAccessionNo = new GeneIdentifierDE.GenBankAccessionNumber();//TODO: add GenBankAccessionNumber
+	      	    GeneIdentifierDE.GenBankAccessionNumber genbankAccessionNo = new GeneIdentifierDE.GenBankAccessionNumber(diffExpSfact.getGeneSymbol()); //TODO: add GenBankAccessionNumber
 //	      	    GeneIdentifierDE.LocusLink locusLinkID = new GeneIdentifierDE.LocusLink(); // add GeneIdentifierDE.LocusLink
 	      		geneResultset.setGeneSymbol(geneSymbol);
+	      		geneResultset.setGenbankAccessionNo(genbankAccessionNo);
 	      		// find out if it has a probeset or a clone associated with it
 	      		//populate ReporterResultset with the approciate one
 	      		ReporterResultset reporterResultset = new ReporterResultset();
-	      		
+	      		//TODO: TEMP PROBESET ID, NEED to SWITCH IT to PROBESET NAME or CLONE NAME
+	      		if(diffExpSfact.getProbesetId() != null ){
+	      			DatumDE reporter = new DatumDE(DatumDE.PROBESET_ID,diffExpSfact.getProbesetId());
+	      			reporterResultset.setReporter(reporter);
+	      		}
+	      		/**
 	      		if(diffExpSfact.getProbesetName() != null && diffExpSfact.getCloneName() == null){
 	      			DatumDE reporter = new DatumDE(DatumDE.PROBESET_ID,diffExpSfact.getProbesetName());
 	      			reporterResultset.setReporter(reporter);
 	      		}
-	      		else{
-	      			DatumDE reporter = new DatumDE(DatumDE.CLONE_ID,diffExpSfact.getProbesetName());
+	      		else if(diffExpSfact.getProbesetName() == null && diffExpSfact.getCloneName() != null){
+	      			DatumDE reporter = new DatumDE(DatumDE.CLONE_ID,diffExpSfact.getCloneName());
 	      			reporterResultset.setReporter(reporter);
 	      		}
+	      		**/
 	      		//find out the disease type associated with the diffExpSfact
 	      		//populate the DiseaseResultset
 	      		DiseaseResultset diseaseResultset = new DiseaseResultset();
-	      		DiseaseNameDE disease = new DiseaseNameDE(diffExpSfact.getADiseaseTypeDim().getDiseaseType());
+	      		//TODO: TEMP DISEASETYPE ID, NEED to SWITCH IT to DISEASE NAME 
+	      		//DiseaseNameDE disease = new DiseaseNameDE(diffExpSfact.getADiseaseTypeDim().getDiseaseType());
+	      		DiseaseNameDE disease = new DiseaseNameDE(diffExpSfact.getDiseaseTypeId().toString());
 	      		diseaseResultset.setDieaseType(disease);
 				//find out the biospecimenID associated with the diffExpSfact
 				//populate the BiospecimenResuluset
+	      		//TODO: TEMP BIOSPECIMEN ID, NEED to SWITCH to SAMPLE_NAME
 				BioSpecimenResultset biospecimenResultset = new BioSpecimenResultset();
-				BioSpecimenIdentifierDE biospecimenID = new BioSpecimenIdentifierDE(diffExpSfact.getABiospecimenDim().getSampleId());
+				//BioSpecimenIdentifierDE biospecimenID = new BioSpecimenIdentifierDE(diffExpSfact.getABiospecimenDim().getSampleId());
+				BioSpecimenIdentifierDE biospecimenID = new BioSpecimenIdentifierDE(diffExpSfact.getBiospecimenId().toString());
 	      		biospecimenResultset.setBiospecimen(biospecimenID);
+	      		DatumDE foldchange = new DatumDE(DatumDE.FOLD_CHANGE,diffExpSfact.getSampleIntensity());
 	      		//add biospecimenresultset to disease etc ...
 	      		diseaseResultset.addBioSpecimenResultset(biospecimenResultset);
 	      		reporterResultset.addDiseaseResultset(diseaseResultset);
@@ -60,8 +73,15 @@ public class ResultsetProcessor {
 	      		//add the reporter to geneResultset
 	      		geneViewContainer.addGeneResultset(geneResultset);
 	      	}
+	       }
 	      }
 
+	}
+	/**
+	 * @return Returns the geneViewContainer.
+	 */
+	public GeneViewContainer getGeneViewContainer() {
+		return geneViewContainer;
 	}
 	public static void main(String[] args) {
 	}
--- src/gov/nih/nci/nautilus/test/ResultsetTest.java
+++ src/gov/nih/nci/nautilus/test/ResultsetTest.java
@@ -10,14 +10,15 @@ import org.apache.ojb.broker.query.QueryByCriteria;
 import org.apache.ojb.broker.query.QueryFactory;
 import gov.nih.nci.nautilus.data.ProbesetDim;
 import gov.nih.nci.nautilus.data.DifferentialExpressionSfact;
-
+import gov.nih.nci.nautilus.resultset.*;
+import gov.nih.nci.nautilus.queryprocessing.ResultsetProcessor;
 import java.util.Collection;
 import java.util.Iterator;
 
 /**
  * @author SahniH
  * Date: September 20, 2004 
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  * This junit test encapsulates the query and resultset tests
  * 
  * 
@@ -25,14 +26,15 @@ import java.util.Iterator;
  * */
 public class ResultsetTest extends TestCase{
     PersistenceBroker broker;
-
+    Collection exprObjects = null;
     private static Class CLASS = ResultsetTest.class;
     public ResultsetTest(String s) {
         super(s);
     }
    	public static junit.framework.Test suite() {	
     	junit.framework.TestSuite suite = new junit.framework.TestSuite();
-        suite.addTest(new ResultsetTest("testCriteria"));
+        //suite.addTest(new ResultsetTest("createQuery"));
+        suite.addTest(new ResultsetTest("testResultset"));
     	return suite;
 	}
 
@@ -46,6 +48,7 @@ public class ResultsetTest extends TestCase{
     {
        try {
            broker = PersistenceBrokerFactory.defaultPersistenceBroker();
+           createQuery();
        } catch (PBFactoryException e) {
            e.printStackTrace();
            fail("Got " + e.getClass().getName() + ": " + e.getMessage());
@@ -60,26 +63,53 @@ public class ResultsetTest extends TestCase{
     /**
 	 * test EqualTo Criteria
 	 */
-    public void testCriteria()
+    public void createQuery()
     {
     	try{
         Criteria exprCrit = new Criteria();
-        exprCrit.addEqualTo("geneSymbol", "CYP2A6");
+        exprCrit.addEqualTo("geneSymbol", "EGFR");
         Query exprQuery = QueryFactory.newQuery(DifferentialExpressionSfact.class, exprCrit);
-        Collection exprObjects = broker.getCollectionByQuery(exprQuery);
-		System.err.println("Got " + exprObjects.size() + " exprObjects objects.");
+        exprObjects = broker.getCollectionByQuery(exprQuery);
+		System.out.println("Got " + exprObjects.size() + " exprObjects objects.");
+        assertNotNull(exprObjects);
+        assertTrue(exprObjects.size() > 0);
+        
 	        for (Iterator iterator = exprObjects.iterator(); iterator.hasNext();) {
-	            DifferentialExpressionSfact expObj = (DifferentialExpressionSfact) iterator.next();
-	            System.out.println("expObj.geneSymbol: " + expObj.getGeneSymbol() + "expObj.probesetName: " +expObj.getProbesetName() );
+	        	DifferentialExpressionSfact expObj = (DifferentialExpressionSfact) iterator.next();
+	            System.out.println("expObj.geneSymbol: " + expObj.getGeneSymbol() + " |expObj.probesetId: " +expObj.getProbesetId()+ " |expObj.dieasesID: " +expObj.getDesId()+ " |expObj.biospecimenID: " +expObj.getBiospecimenId() );
 	        }
  		} catch (Exception ex) {
  			ex.printStackTrace();
  			fail("Got " + ex.getClass().getName() + ": " + ex.getMessage());
- 		}
-
-       /*
-        assertNotNull(results);
-        assertTrue(results.size() > 0);*/
-
+ 		}  
+    }
+    public void testResultset(){
+    	ResultsetProcessor resultsetProc = new ResultsetProcessor();
+    	assertNotNull(exprObjects);
+        assertTrue(exprObjects.size() > 0);
+    	resultsetProc.createGeneView(exprObjects);
+    	GeneViewContainer geneViewContainer = resultsetProc.getGeneViewContainer();
+    	Collection genes = geneViewContainer.getGeneResultsets();
+    	for (Iterator geneIterator = genes.iterator(); geneIterator.hasNext();) {
+    		GeneResultset geneResultset = (GeneResultset)geneIterator.next();
+    		Collection reporters = geneResultset.getReporterResultsets();
+    		for (Iterator reporterIterator = reporters.iterator(); reporterIterator.hasNext();) {
+        		ReporterResultset reporterResultset = (ReporterResultset)reporterIterator.next();
+        		Collection diseaseTypes = reporterResultset.getDiseaseResultset();
+        		for (Iterator diseaseIterator = diseaseTypes.iterator(); diseaseIterator.hasNext();) {
+        			DiseaseResultset dieaseResultset = (DiseaseResultset)diseaseIterator.next();
+            		Collection biospecimens = dieaseResultset.getBioSpecimenResultsets();
+            		for (Iterator biospecimenIterator = biospecimens.iterator(); biospecimenIterator.hasNext();) {
+            			BioSpecimenResultset biospecimenResultset = (BioSpecimenResultset)biospecimenIterator.next();
+                	            		System.out.println(	"GeneSymbol: "+geneResultset.getGeneSymbol().getValueObject().toString()+
+            							"| ReporterId: "+ reporterResultset.getReporter().getValue().toString()+
+										"| DieaseType Id: "+dieaseResultset.getDieaseType().getValue().toString()+
+										"| BioSpecimen Id: "+ biospecimenResultset.getBiospecimen().getValue().toString()//+
+										/**"| FoldChange Value"+ biospecimenResultset.getFolgChangeValue().getValue().toString()*/);
+            		}
+        		}
+    		}
+    	}
+    	
     }
 }
