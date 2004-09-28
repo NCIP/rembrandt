b40d8c15b6fdef3592e915568920c80250901461
Ram Bhattaru <bhattarr>
2004-09-28 22:59:58 +0000

    Made GeneOntologyID an Integer from String
    SVN-Revision: 155

--- src/gov/nih/nci/nautilus/de/GeneOntologyDE.java
+++ src/gov/nih/nci/nautilus/de/GeneOntologyDE.java
@@ -5,16 +5,16 @@ import gov.nih.nci.nautilus.util.ApplicationContext;
 
 
 /**
- * This  class encapsulates the properties of an caintergator 
+ * This  class encapsulates the properties of an caintergator
  * GeneOntologyDE object.
- *  
+ *
  * Dana Zhang
- * Date: August 12, 2004 
+ * Date: August 12, 2004
  * Version 1.0
  */
 public class GeneOntologyDE extends DomainElement{
-   
-    
+
+
   // ****************************************************
   //                   CONSTRUCTOR(S)
   // ****************************************************
@@ -22,34 +22,23 @@ public class GeneOntologyDE extends DomainElement{
    /**
     * Initializes a newly created <code>GeneOntologyDE</code> object so that it represents an GeneOntologyDE.
     */
-    public GeneOntologyDE(String ontologyID) {
+    public GeneOntologyDE(Integer ontologyID) {
         super(ontologyID);
     }
-   
 
-  /**
-    * Sets the value for this <code>GeneOntologyDE</code> object
-    * @param object the value    
-	*/  	
+
     public void setValue(Object obj) throws Exception {
-        if (! (obj instanceof String) )
+        if (! (obj instanceof Integer) )
             throw new Exception ( "Could not set the value.  Parameter is of invalid data type: " + obj);
-        setValueObject((String)obj);
+        setValueObject((Integer)obj);
     }
 
-  /**
-    * Returns the ontologyID for this GeneOntologyDE obect.
-    * @return the ontologyID for this <code>GeneOntologyDE</code> object
-    */	
-    public String getValueObject() {
-        return (String) getValue();
+    public Integer getValueObject() {
+        return (Integer) getValue();
     }
 
-  /**
-    * Sets the probeSetID for this <code>GeneOntologyDE</code> object
-    * @param probeSetID the probeSetID    
-	*/ 
-    public void setValueObject(String ontologyID) {
+
+    public void setValueObject(Integer ontologyID) {
 	  if(ontologyID != null){
         value = ontologyID;
 		}
