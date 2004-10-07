1fef92f0da7305babee761562c74383fab673d36
Ram Bhattaru <bhattarr>
2004-10-07 15:36:16 +0000

    no message
    SVN-Revision: 196

---
+++ src/gov/nih/nci/nautilus/resultset/ResultSet.java
@@ -0,0 +1,12 @@
+package gov.nih.nci.nautilus.resultset;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Ram
+ * Date: Oct 6, 2004
+ * Time: 3:34:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface ResultSet {
+    // it is simply a marker interface for now
+}
