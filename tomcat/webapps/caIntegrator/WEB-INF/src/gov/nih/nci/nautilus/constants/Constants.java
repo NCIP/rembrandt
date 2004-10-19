64cb8b6c5659a67e624c63aac9bb0cf352441a59
Priti Shah <Priti.Shah@AoA.hhs.gov>
2004-10-19 22:05:11 +0000

    Added Key for Resultset in session
    SVN-Revision: 291

--- tomcat/webapps/caIntegrator/WEB-INF/src/gov/nih/nci/nautilus/constants/Constants.java
+++ tomcat/webapps/caIntegrator/WEB-INF/src/gov/nih/nci/nautilus/constants/Constants.java
@@ -3,4 +3,5 @@ package gov.nih.nci.nautilus.constants;
 
 public final class Constants {
 	public static final String QUERY_KEY = "nautilus.queryMap";
+	public static final String RESULTSET_KEY = "nautilus.queryMap";
 		}
