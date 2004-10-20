c2db8357c35296ba7e16c56f8c66e6c5765acb80
no_author <no_author@no_author.cvs2svn>
2004-10-20 04:16:59 +0000

    This commit was manufactured by cvs2svn to create tag 'caintegrator_x_x_nightly__build_7_'.
    SVN-Revision: 293

--- tomcat/webapps/caIntegrator/WEB-INF/src/gov/nih/nci/nautilus/constants/Constants.java
+++
@@ -1,7 +0,0 @@
-package gov.nih.nci.nautilus.constants;
-
-
-public final class Constants {
-	public static final String QUERY_KEY = "nautilus.queryMap";
-	public static final String RESULTSET_KEY = "nautilus.queryMap";
-		}
