df8dc3a039b9791a0aeb7f1543d9091d68ef10df
Himanso Sahni <Himanso.Sahni@fda.hhs.gov>
2004-11-01 21:01:52 +0000

    add Copy Number View
    SVN-Revision: 391

--- src/gov/nih/nci/nautilus/view/ViewType.java
+++ src/gov/nih/nci/nautilus/view/ViewType.java
@@ -11,6 +11,7 @@ abstract public class ViewType {
     abstract ViewType getViewType();
     public final static GeneSingleSampleView GENE_SINGLE_SAMPLE_VIEW = new GeneSingleSampleView();
     public final static GeneGroupSampleView GENE_GROUP_SAMPLE_VIEW = new GeneGroupSampleView();
+    public final static CopyNumberSampleView COPYNUMBER_GROUP_SAMPLE_VIEW = new CopyNumberSampleView();
     public final static ClinicalView CLINICAL_VIEW = new ClinicalView();
 
     public static class GeneSingleSampleView extends ViewType {
@@ -28,4 +29,8 @@ abstract public class ViewType {
            return CLINICAL_VIEW ;
        }
     }
+    public static class CopyNumberSampleView extends ViewType {
+        public ViewType getViewType() {
+            return COPYNUMBER_GROUP_SAMPLE_VIEW;
+        }
 }
