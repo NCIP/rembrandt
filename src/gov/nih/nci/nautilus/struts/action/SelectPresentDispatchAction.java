9859e16feaf85c06a15b712d509a6c39d953efb6
Priti Shah <Priti.Shah@AoA.hhs.gov>
2004-11-08 15:49:24 +0000

    Modified the Presentation page
    SVN-Revision: 458

--- src/gov/nih/nci/nautilus/struts/action/SelectPresentDispatchAction.java
+++ src/gov/nih/nci/nautilus/struts/action/SelectPresentDispatchAction.java
@@ -15,8 +15,17 @@ import org.apache.struts.action.ActionMapping;
 import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionError;
 
+import gov.nih.nci.nautilus.struts.form.*;
+
+import gov.nih.nci.nautilus.de.*;
+import gov.nih.nci.nautilus.resultset.ResultSet;
 import gov.nih.nci.nautilus.query.*;
 import gov.nih.nci.nautilus.constants.Constants;
+import gov.nih.nci.nautilus.queryprocessing.ge.GeneExpr;
+import gov.nih.nci.nautilus.view.*;
+import java.util.*;
+
+
 
 
 
@@ -34,7 +43,8 @@ public class SelectPresentDispatchAction extends DispatchAction {
 	 * @return ActionForward
 	 * @throws Exception
 	 */
-	public ActionForward changegenelist(
+
+	public ActionForward runReport(
 		ActionMapping mapping,
 		ActionForm form,
 		HttpServletRequest request,
@@ -42,41 +52,80 @@ public class SelectPresentDispatchAction extends DispatchAction {
 		throws Exception {
 
 		ActionErrors errors = new ActionErrors();
-
+		
+		SelectPresentationForm thisForm = (SelectPresentationForm) form;
+		
+		DomainElementClass [] allDomainElems = thisForm.getSelectedElements();
+		Collection selectedDomainElems = new ArrayList();
+		String [] selectedIndexes = thisForm.getListTo();
 		QueryCollection queryCollect = (QueryCollection) request.getSession().getAttribute(Constants.QUERY_KEY);
+		
+		for (int i = 0; i < selectedIndexes.length; i++) {
+			int selectedIndex = Integer.parseInt(selectedIndexes[i]);
+			selectedDomainElems.add(allDomainElems[selectedIndex]);
+		}
 
-// Code used to test Finalize query page in the absense of QueryCollection coming from menu!!
-//		queryCollect = new QueryCollection();
-//		GeneExpressionQuery q1 = (GeneExpressionQuery) QueryManager.createQuery(QueryType.GENE_EXPR_QUERY_TYPE);
-//		GeneExpressionQuery q2 = (GeneExpressionQuery) QueryManager.createQuery(QueryType.GENE_EXPR_QUERY_TYPE);
-//		GeneExpressionQuery q3 = (GeneExpressionQuery) QueryManager.createQuery(QueryType.GENE_EXPR_QUERY_TYPE);
-//		q1.setQueryName("Query 1");
-//		q2.setQueryName("Query 2");
-//		q3.setQueryName("Query 3");
-//		queryCollect.putQuery(q1);
-//		queryCollect.putQuery(q2);
-//		queryCollect.putQuery(q3);
-//		request.getSession().setAttribute(Constants.QUERY_KEY, queryCollect);
-//		Code used to test Finalize query page ends!!
+		// Get View from QueryCollection and set selected elements in the View
+		if (queryCollect != null) { 
+			if (queryCollect.hasCompoundQuery()) {
 
-		if (queryCollect != null) {
-			if (queryCollect.hasQuery()) {
-					ActionForward thisForward = mapping.findForward("success");
-					return thisForward;
-				}
-			else {
-				System.out.println("QueryCollection has no queries.  Please select a query to execute");
-				errors.add(ActionErrors.GLOBAL_ERROR, new ActionError("gov.nih.nci.nautilus.struts.action.refinequery.querycoll.no.error"));
-				this.saveErrors(request, errors);
+						try {
+							// Get View from Query Collection that user selected
+							Viewable thisView = queryCollect.getCompoundQuery().getAssociatedView();
+							// Set the selected presentation elements in the View
+							((View) thisView).setSelectedElements(selectedDomainElems);
+							
+							// Execute the query and place the query in session
+//							ResultSet[] queryResultSetObjects = QueryManager.executeQuery(queryCollect.getCompoundQuery());
+//							print(queryResultSetObjects);
+//							request.getSession().setAttribute(Constants.RESULTSET_KEY,queryResultSetObjects);
+							
+							ActionForward thisForward = mapping.findForward("success");
+							return thisForward;
+						}
+						catch (Exception e) {
+							e.printStackTrace();
+							errors.add(ActionErrors.GLOBAL_ERROR, new ActionError(e.getMessage()));
+							this.saveErrors(request, errors);
+						}
+
+
+				} else {
+						System.out.println("SelectPresentationForm - QueryCollection does not have a CompoundQuery");
+						errors.add(ActionErrors.GLOBAL_ERROR, new ActionError("gov.nih.nci.nautilus.struts.action.executequery.querycoll.no.error"));
+						this.saveErrors(request, errors);
 			}
-		}else{
-			System.out.println("QueryCollection object missing in session!!");
+		} else {
+			System.out.println("SelectPresentationForm - Query Collection is null");
 			errors.add(ActionErrors.GLOBAL_ERROR, new ActionError("gov.nih.nci.nautilus.struts.action.refinequery.querycoll.missing.error"));
 			this.saveErrors(request, errors);
 		}
-		ActionForward thisForward = mapping.findForward("failure");
+		
+		// Test if values set correctly
+//		Viewable thisView = queryCollect.getCompoundQuery().getAssociatedView();
+//		ArrayList aList = (ArrayList) ((View) thisView).getSelectedElements();
+//		for (Iterator iter = aList.iterator(); iter.hasNext();) {
+//			DomainElementClass element = (DomainElementClass) iter.next();
+//			System.out.println("Domain element ===>"+element.getName()+ " "+element.getLabel());
+//		}
+
+		ActionForward thisForward = mapping.findForward("backtopresent");
 
 		return thisForward;
 
-     }
+	 }
+
+	private void print(ResultSet[] geneExprObjects) {
+		if(geneExprObjects != null){
+			System.out.println("Number of Records:"+ geneExprObjects.length);
+			for (int i =0; i < geneExprObjects.length; i++) {
+				GeneExpr.GeneExprSingle expObj = (GeneExpr.GeneExprSingle) geneExprObjects[i];
+				if(expObj != null){
+				System.out.println( "uID: " + expObj.getDesId() + "|geneSymbol: " + expObj.getGeneSymbol() +"|clone: " + expObj.getCloneName()+"|probeSet: "+expObj.getProbesetName()+"|biospecimenID: " + expObj.getBiospecimenId() );
+				}
+			}
+		}
+		
+	}
+	
 }
