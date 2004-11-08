3abf9293eb961fc19b8a792901f51fa4414c0a5c
Priti Shah <Priti.Shah@AoA.hhs.gov>
2004-11-08 15:51:27 +0000

    Modifications for RefineQuery page
    SVN-Revision: 459

--- src/gov/nih/nci/nautilus/constants/Constants.java
+++ src/gov/nih/nci/nautilus/constants/Constants.java
@@ -4,4 +4,6 @@ package gov.nih.nci.nautilus.constants;
 public final class Constants {
 	public static final String QUERY_KEY = "nautilus.queryMap";
 	public static final String RESULTSET_KEY = "geneViewResultSet";
+	public static final String VALID_QUERY_TYPES_KEY = "nautilus.request.validQuerytypes";
+	
 		}
