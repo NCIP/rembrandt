1a2fd30ae54f449805697203e4a4ce9a0ee19d7e
Himanso Sahni <Himanso.Sahni@fda.hhs.gov>
2004-11-15 21:20:15 +0000

    intial check in
    SVN-Revision: 597

--- src/gov/nih/nci/nautilus/test/KaplanMeierPlotContainerTest.java
+++ src/gov/nih/nci/nautilus/test/KaplanMeierPlotContainerTest.java
@@ -1,6 +1,6 @@
 /*
  *  @author: SahniH
- *  Created on Nov 10, 2004
+ *  Created on Nov 15, 2004
  *  @version $ Revision: 1.0 $
  * 
  *	The caBIO Software License, Version 1.0
@@ -49,36 +49,37 @@
  */
 package gov.nih.nci.nautilus.test;
 
+import java.util.Collection;
+import java.util.Iterator;
+
 import gov.nih.nci.nautilus.criteria.ArrayPlatformCriteria;
 import gov.nih.nci.nautilus.criteria.Constants;
 import gov.nih.nci.nautilus.criteria.GeneIDCriteria;
 import gov.nih.nci.nautilus.de.ArrayPlatformDE;
+import gov.nih.nci.nautilus.de.ExprFoldChangeDE;
 import gov.nih.nci.nautilus.de.GeneIdentifierDE;
+import gov.nih.nci.nautilus.graph.kaplanMeier.KMDrawingPoint;
+import gov.nih.nci.nautilus.graph.kaplanMeier.KaplanMeier;
 import gov.nih.nci.nautilus.query.GeneExpressionQuery;
 import gov.nih.nci.nautilus.query.QueryManager;
 import gov.nih.nci.nautilus.query.QueryType;
 import gov.nih.nci.nautilus.resultset.Resultant;
 import gov.nih.nci.nautilus.resultset.ResultsContainer;
 import gov.nih.nci.nautilus.resultset.ResultsetManager;
-import gov.nih.nci.nautilus.resultset.geneExpressionPlot.DiseaseGeneExprPlotResultset;
-import gov.nih.nci.nautilus.resultset.geneExpressionPlot.GeneExprDiseasePlotContainer;
-import gov.nih.nci.nautilus.resultset.geneExpressionPlot.ReporterFoldChangeValuesResultset;
+import gov.nih.nci.nautilus.resultset.kaplanMeierPlot.KaplanMeierPlotContainer;
+import gov.nih.nci.nautilus.resultset.kaplanMeierPlot.SampleKaplanMeierPlotResultset;
 import gov.nih.nci.nautilus.view.ViewFactory;
 import gov.nih.nci.nautilus.view.ViewType;
-
-import java.text.DecimalFormat;
-import java.util.Collection;
-import java.util.Iterator;
-
 import junit.framework.TestCase;
 
 /**
  * @author SahniH
- * Date: Nov 10, 2004
+ * Date: Nov 15, 2004
  * 
  */
-public class GeneExpressionPlotTest extends TestCase {
+public class KaplanMeierPlotContainerTest extends TestCase {
 	GeneExpressionQuery geneQuery;
+
 	/*
 	 * @see TestCase#setUp()
 	 */
@@ -92,32 +93,39 @@ public class GeneExpressionPlotTest extends TestCase {
 	protected void tearDown() throws Exception {
 		super.tearDown();
 	}
-    private void buildGeneExprDiseasePlotQuery(){
+    private void buildKaplanMeierPlotQuery(){
         GeneIDCriteria geneCrit = new GeneIDCriteria();
         geneCrit.setGeneIdentifier(new GeneIdentifierDE.GeneSymbol("VEGF"));
         geneQuery = (GeneExpressionQuery) QueryManager.createQuery(QueryType.GENE_EXPR_QUERY_TYPE);
-        geneQuery.setQueryName("GeneExpressionPlot");
-        geneQuery.setAssociatedView(ViewFactory.newView(ViewType.GENE_GROUP_SAMPLE_VIEW));
+        geneQuery.setQueryName("KaplanMeierPlot");
+        geneQuery.setAssociatedView(ViewFactory.newView(ViewType.GENE_SINGLE_SAMPLE_VIEW));
         geneQuery.setGeneIDCrit(geneCrit);
         geneQuery.setArrayPlatformCrit(new ArrayPlatformCriteria(new ArrayPlatformDE(Constants.AFFY_OLIGO_PLATFORM)));
     }
-	public void testGeneExprDiseaseView(){
+
+	/**
+	 * @param geneExprDiseasePlotContainer
+	 */
+
+	public void testGetSampleKaplanMeierPlotResultsets() {
 		//test Single Query
 		try {
-			buildGeneExprDiseasePlotQuery();
-			System.out.println("Building  Gene Expression Plot Query>>>>>>>>>>>>>>>>>>>>>>>");
-			Resultant resultant = ResultsetManager.executeGeneExpressPlotQuery(geneQuery);
-			//System.out.println("DiseaseQuery:\n"+ geneQuery.toString());
-			//assertNotNull(resultant);
+			buildKaplanMeierPlotQuery();
+			System.out.println("Building  Kaplan Meier Plot Query>>>>>>>>>>>>>>>>>>>>>>>");
+			Resultant resultant = ResultsetManager.executeKaplanMeierPlotQuery(geneQuery);
 			if(resultant != null){
-				System.out.println("Testing Disease Gene Query >>>>>>>>>>>>>>>>>>>>>>>");
-				//System.out.println("Associated Query/n"+resultant.getAssociatedQuery());
+				System.out.println("Testing DbuildKaplanMeierPlotQuery Query >>>>>>>>>>>>>>>>>>>>>>>");
 				ResultsContainer resultsContainer = resultant.getResultsContainer();
 				System.out.println("Associated ViewType/n"+resultant.getAssociatedView());
-				if (resultsContainer instanceof GeneExprDiseasePlotContainer){
-					GeneExprDiseasePlotContainer geneExprDiseasePlotContainer = (GeneExprDiseasePlotContainer) resultsContainer;
-			        displayGeneExprPlotData(geneExprDiseasePlotContainer);
-
+				if (resultsContainer instanceof KaplanMeierPlotContainer){
+					KaplanMeierPlotContainer kaplanMeierPlotContainer = (KaplanMeierPlotContainer) resultsContainer;
+					System.out.println(">>>>Display All samples");
+			        display(kaplanMeierPlotContainer.getBioSpecimenResultsets());
+			        displatKMDrawingPoint(kaplanMeierPlotContainer.getBioSpecimenResultsets());
+					System.out.println(">>>>Display Gene Express for UP Regulated 3 Folds");
+			        ExprFoldChangeDE exprFoldChangeDE = new ExprFoldChangeDE.UpRegulation(new Float(3.0));
+		    		display(kaplanMeierPlotContainer.getSampleKaplanMeierPlotResultsets(exprFoldChangeDE));
+			        displatKMDrawingPoint(kaplanMeierPlotContainer.getSampleKaplanMeierPlotResultsets(exprFoldChangeDE));
 				}
 			}
 		} catch (Exception e) {
@@ -125,39 +133,38 @@ public class GeneExpressionPlotTest extends TestCase {
 			e.printStackTrace();
 		}			
 
-}
+	}
+
 
 	/**
-	 * @param geneExprDiseasePlotContainer
+	 * @param kaplanMeierPlotContainer
 	 */
-	private void displayGeneExprPlotData(GeneExprDiseasePlotContainer geneExprDiseasePlotContainer) {
-		System.out.println("inside display diease");
-		final DecimalFormat resultFormat = new DecimalFormat("0.00");	
-		final DecimalFormat pValueFormat = new DecimalFormat("0.0000");	
-		assertNotNull(geneExprDiseasePlotContainer);
-		System.out.println("Gene:"+geneExprDiseasePlotContainer.getGeneSymbol());
-    	Collection diseases = geneExprDiseasePlotContainer.getDiseaseGeneExprPlotResultsets();
-    	StringBuffer header = new StringBuffer();
-        StringBuffer stringBuffer = new StringBuffer();
-    	//get group size (as Disease or Agegroup )from label.size
-        String label = null;
-    	
-        //set up the header for the table
-    	header.append("Diseases\tReporter Name\tIntensity\tPvalue");
-		System.out.println(header.toString());
-    	for (Iterator diseasesIterator = diseases.iterator(); diseasesIterator.hasNext();) {
-    		DiseaseGeneExprPlotResultset diseaseResultset = (DiseaseGeneExprPlotResultset)diseasesIterator.next();
-    		String diseaseName = diseaseResultset.getType().getValue().toString();
-    		stringBuffer.append(diseaseName+"\n");
-    		Collection reporters = diseaseResultset.getReporterFoldChangeValuesResultsets(); //geneResultset.getReporterResultsets();
-    		for (Iterator reporterIterator = reporters.iterator(); reporterIterator.hasNext();) {
-    			ReporterFoldChangeValuesResultset reporterResultset = (ReporterFoldChangeValuesResultset)reporterIterator.next();
-        		String reporterName = reporterResultset.getReporter().getValue().toString();
-       			Double intensityValue = (Double)reporterResultset.getFoldChangeIntensity().getValue();
-       			Double pvalue = (Double)reporterResultset.getRatioPval().getValue();
-       			stringBuffer.append(reporterName+"\t"+resultFormat.format(intensityValue)+"\t"+pValueFormat.format(pvalue)+"\n");  
-    		}
-    	}
-    	System.out.println(stringBuffer.toString());		
+	private void display(Collection samples) {
+		if(samples != null){
+			System.out.println("Number of Gene Expression Sample:"+samples.size());
+    		System.out.println("ID: \tSurvival Length: \tCensor Status:" );
+
+		   	for (Iterator sampleIterator = samples.iterator(); sampleIterator.hasNext();) {
+	    		SampleKaplanMeierPlotResultset sample = (SampleKaplanMeierPlotResultset)sampleIterator.next();
+	    		System.out.println(sample.getBiospecimen().getValue()+
+									" \t"+sample.getSurvivalLength().getValue()+
+	    							" \t"+sample.getCensor().getValue());
+		   	}
+		}
+		
 	}
+
+
+	public void displatKMDrawingPoint(Collection samples) {
+		KaplanMeier km = new KaplanMeier(samples); 
+		KMDrawingPoint[] points = km.getDrawingPoints(); 
+		System.out.println("\nOutput points: "); 
+		for (int i=0; i<points.length; i++) {
+			System.out.println(points[i].getX() 
+							+ "\t" + points[i].getY()
+							+ "\t" + points[i].isChecked()); 
+		}
+
+	}
+
 }
