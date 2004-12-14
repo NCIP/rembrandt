ac6f88d4836a6e6cfc5e4a440941e4f43f00a9a9
Ryan Landy <landyr>
2004-12-14 16:55:45 +0000

    pop up big fix
    SVN-Revision: 940

--- src/gov/nih/nci/nautilus/struts/action/ReportGeneratorAction.java
+++ src/gov/nih/nci/nautilus/struts/action/ReportGeneratorAction.java
@@ -18,6 +18,8 @@ import org.apache.struts.actions.DispatchAction;
 
 public class ReportGeneratorAction extends DispatchAction {
 
+	public String goback = "";
+	
     public ActionForward compundReport(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
@@ -36,6 +38,30 @@ public class ReportGeneratorAction extends DispatchAction {
             
         }
         request.setAttribute(NautilusConstants.QUERY_KEY,queryCollection);
-        return mapping.findForward("success");
+        //we've gotten this far, so we're validated. find failure to refresh page
+        //and set att of the req to spawn report prev
+        request.setAttribute("preview", new String("yes"));
+        System.out.println("back: " + this.goback);
+        return mapping.findForward(this.goback);
+ //       return mapping.findForward("success");
+	}
+	
+	public ActionForward previewReportGene(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+        this.goback = "backToGeneExp";
+		return previewReport(mapping, form, request, response);
+	}
+	public ActionForward previewReportCGH(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		this.goback = "backToCGH";
+		return previewReport(mapping, form, request, response);
+	}
+	public ActionForward previewReportClinical(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		this.goback = "backToClinical";
+		return previewReport(mapping, form, request, response);
 	}
 }
