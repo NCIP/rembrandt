81a82021ed66f3ccdde9c09a7b8ee79f122a8ffc
no_author <no_author@no_author.cvs2svn>
2005-01-03 20:47:50 +0000

    This commit was manufactured by cvs2svn to create tag 'caintegrator_0_5_0_build_4_'.
    SVN-Revision: 1007

---
+++ src/gov/nih/nci/nautilus/struts/action/DoFirst.java
@@ -0,0 +1,15 @@
+
+package gov.nih.nci.nautilus.struts.action;
+import org.apache.struts.action.*;
+import javax.servlet.http.*;
+
+public class DoFirst extends Action {
+	public ActionForward perform(ActionMapping aMapping, ActionForm aForm,
+			HttpServletRequest aRequest, HttpServletResponse aResponse) {
+		aRequest.getSession().setAttribute("currentPage", "0");
+		return aMapping.findForward("success");
+
+	}
+
+}
+
