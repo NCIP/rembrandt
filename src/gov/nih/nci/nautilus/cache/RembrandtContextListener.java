9e915bbd2fcefde8b2511051ca996a95cb145d43
bauerd <bauerd>
2005-02-28 16:04:19 +0000

    added comments
    SVN-Revision: 1159

--- src/gov/nih/nci/nautilus/cache/RembrandtContextListener.java
+++ src/gov/nih/nci/nautilus/cache/RembrandtContextListener.java
@@ -3,19 +3,29 @@ package gov.nih.nci.nautilus.cache;
 import javax.servlet.ServletContextEvent; 
 import javax.servlet.ServletContextListener;
 /**
- * This listener is used by the application to be notified of 
+ * This listener has one purpose and that is to notify the
+ * SessionTracker class when the application context has been 
+ * loaded or unloaded. Check the application Web.xml for the
+ * intialization of this listener.
+ * 
  * @author BauerD
  * Feb 17, 2005
  * 
  */
 public class RembrandtContextListener implements ServletContextListener {
-
+	/** 
+	 * this method is fired whenever application server loads the context
+	 * that this listener is added to in the web.xml
+	 */
 	public void contextInitialized(ServletContextEvent arg0) {
 		SessionTracker.setAppplicationRunning(true);
 
 	}
 
-	
+	/**
+	 * this method is fired whenever the application server unloads 
+	 * the context that this listener is added to in the web.xml
+	 */
 	public void contextDestroyed(ServletContextEvent arg0) {
 		SessionTracker.setAppplicationRunning(false);
 
