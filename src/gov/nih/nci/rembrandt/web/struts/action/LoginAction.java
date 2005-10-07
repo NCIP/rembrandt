573c72cdd9e0adbf5521b645dff2d4a5b85338eb
Kevin Rosso <Kevin.Rosso@fda.hhs.gov>
2005-10-07 14:22:41 +0000

    csm login
    SVN-Revision: 1672

--- src/gov/nih/nci/rembrandt/web/struts/action/LoginAction.java
+++ src/gov/nih/nci/rembrandt/web/struts/action/LoginAction.java
@@ -20,72 +20,79 @@ import org.apache.struts.action.ActionMapping;
 public final class LoginAction extends Action
 {
     private static Logger logger = Logger.getLogger(RembrandtConstants.LOGGER);
-	public ActionForward perform(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-	{
-		LoginForm f = (LoginForm) form;
-		String userName=f.getUserName();
-		String password = f.getPassword();
-		
-		boolean valid = false;		
-		//load the props file
-		Properties props = new Properties();
-		HttpSession session = request.getSession();
-		ServletContext context = session.getServletContext();
+    public ActionForward perform(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+    {
+        HttpSession session = request.getSession();
+        ServletContext context = session.getServletContext();
+        LoginForm f = (LoginForm) form;
+        if(f.getUserLoggedIn()){
+            session.setAttribute("logged", "yes");
+            session.setAttribute("name", f.getUserName());
+            return (mapping.findForward("success"));
+        }
+        else
+            return (mapping.findForward("failure"));
+        
+        
+    }
+        
+        /**old login method...should be deleted when time is appropriate
+         * 
+         * -kevin rosso
+        LoginForm f = (LoginForm) form;
+        String userName=f.getUserName();
+        String password = f.getPassword();
+        
+        boolean valid = false;
+        
+        //load the props file
+        Properties props = new Properties();
+        HttpSession session = request.getSession();
+        ServletContext context = session.getServletContext();
 
-	    try {
-	        logger.debug("loading props...");
-	   		props.load(new FileInputStream(context.getRealPath("WEB-INF")+"/users.properties"));
-	
-		   
-	   		logger.debug("props file length: " + props.size());
-	
-			int accounts = Integer.parseInt(props.getProperty("accounts"));
-			String u = "";
-			String p = "";
-			String r = "";
-			String n = "";
-	
-			
-			for(int t=1; t<accounts+1; t++)	{
-				u = props.getProperty("User"+t);
-				p = props.getProperty("Pass"+t);
-				r = props.getProperty("Role"+t);
-				n = props.getProperty("Name"+t);
-				
-				if(userName.equals(u) && password.equals(p))
-				{
-					//set something in the session here
-					valid = true;
+        try {
+            logger.debug("loading props...");
+            props.load(new FileInputStream(context.getRealPath("WEB-INF")+"/users.properties"));
+    
+           
+            logger.debug("props file length: " + props.size());
+    
+            int accounts = Integer.parseInt(props.getProperty("accounts"));
+            String u = "";
+            String p = "";
+            String r = "";
+            String n = "";
+    
+            
+            for(int t=1; t<accounts+1; t++) {
+                u = props.getProperty("User"+t);
+                p = props.getProperty("Pass"+t);
+                r = props.getProperty("Role"+t);
+                n = props.getProperty("Name"+t);
+                
+                if(userName.equals(u) && password.equals(p))
+                {
+                    //set something in the session here
+                    valid = true;
 
-					session.setAttribute("logged", "yes");
-					session.setAttribute("name", n);
-					session.setAttribute("role", r);
-					return (mapping.findForward("success"));	
-				}
-			}
-		} 
-		catch (Exception e) {
-		    logger.error("Can't read user props");
-		    logger.error(e);
-		}
-		
-		if(valid)
-			return (mapping.findForward("success"));
-		else
-			return (mapping.findForward("failure"));
-			
-		/*
-		if(userName.equals("admin") && password.equals("admin123"))
-		{
-		//set something in the session here
-		HttpSession session = request.getSession();
-		session.setAttribute("logged", "yes");
-		return (mapping.findForward("success"));			
-		}
-
-		else
-		return (mapping.findForward("failure"));
-		*/
-	}
+                    session.setAttribute("logged", "yes");
+                    session.setAttribute("name", n);
+                    session.setAttribute("role", r);
+                    return (mapping.findForward("success"));    
+                }
+            }
+        } 
+        catch (Exception e) {
+            logger.error("Can't read user props");
+            logger.error(e);
+        }
+        
+        if(valid)
+            return (mapping.findForward("success"));
+        else
+            return (mapping.findForward("failure"));
+            
+        
+    }**/
 }
--- src/gov/nih/nci/rembrandt/web/struts/form/LoginForm.java
+++ src/gov/nih/nci/rembrandt/web/struts/form/LoginForm.java
@@ -1,4 +1,6 @@
 package gov.nih.nci.rembrandt.web.struts.form;
+import gov.nih.nci.rembrandt.web.struts.form.UIFormValidator;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.struts.action.ActionError;
@@ -8,34 +10,46 @@ import org.apache.struts.action.ActionMapping;
 
 public final class LoginForm extends ActionForm
 {
-
+private boolean userLoggedIn = false;    
 private String userName;
 private String password;
+
+public boolean getUserLoggedIn(){
+    return userLoggedIn;
+}
+
 public void setUserName(String argUserName)
 {
-	userName = argUserName;
+    userName = argUserName;
 }
 public String getUserName()
 {
-	return userName;
+    return userName;
 }
 public void setPassword(String argPassword)
 {
-	password = argPassword;
+    password = argPassword;
 }
 public String getPassword()
 {
-	return password;
+    return password;
 }
 public ActionErrors validate(ActionMapping mapping,
-			HttpServletRequest request)
+            HttpServletRequest request)
 {
-	ActionErrors errors=new ActionErrors();
-	if(userName == null || password.equals(""))
-	{
-		errors.add("User Name:",new ActionError("error.userName"));
-		errors.add("Password:",new ActionError("error.password"));
-	}
-	return errors;
+    ActionErrors errors=new ActionErrors();
+    if(userName == null || password.equals(""))
+    {
+        errors.add("User Name:",new ActionError("error.userName"));
+        errors.add("Password:",new ActionError("error.password"));
+    }
+    errors = UIFormValidator.validateLDAP(userName, password, errors);
+    
+    if (errors.isEmpty()) {
+        userLoggedIn = true;
+    }
+    
+    return errors;
 }
+
 }
