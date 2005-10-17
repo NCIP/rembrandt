a83e425b423a5c5b1ec020ce2fd795f790cbe071
Himanso Sahni <Himanso.Sahni@fda.hhs.gov>
2005-10-17 13:10:51 +0000

    Initial Check in
    SVN-Revision: 1695

---
+++ src/gov/nih/nci/rembrandt/service/findings/FindingsFactory.java
@@ -0,0 +1,110 @@
+/**
+ * 
+ */
+package gov.nih.nci.rembrandt.service.findings;
+
+import gov.nih.nci.caintegrator.dto.finding.ClassComparisonFindingsResultset;
+
+import gov.nih.nci.caintegrator.dto.query.Query;
+import gov.nih.nci.caintegrator.exceptions.FindingsAnalysisException;
+import gov.nih.nci.caintegrator.exceptions.FindingsQueryException;
+import gov.nih.nci.caintegrator.exceptions.ValidationException;
+import gov.nih.nci.caintegrator.service.findings.ClinicalFinding;
+import gov.nih.nci.caintegrator.service.findings.CopyNumberFinding;
+import gov.nih.nci.caintegrator.service.findings.GEIntensityFinding;
+import gov.nih.nci.caintegrator.service.findings.HCAFinding;
+import gov.nih.nci.caintegrator.service.findings.KMFinding;
+import gov.nih.nci.caintegrator.service.findings.PCAFinding;
+import gov.nih.nci.rembrandt.dto.query.ClassComparisonQuery;
+import gov.nih.nci.rembrandt.service.findings.strategies.ClassComparisonFindingStrategy;
+
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author sahnih
+ *
+ */
+public class FindingsFactory implements
+		gov.nih.nci.caintegrator.service.findings.FindingsFactory {
+	private Logger logger = Logger.getLogger(FindingsFactory.class);
+	/* (non-Javadoc)
+	 * @see gov.nih.nci.caintegrator.service.findings.FindingsFactory#createKMFinding(gov.nih.nci.caintegrator.dto.query.Query)
+	 */
+	public KMFinding createKMFinding(Query query) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see gov.nih.nci.caintegrator.service.findings.FindingsFactory#createCopyNumberFinding(gov.nih.nci.caintegrator.dto.query.Query)
+	 */
+	public CopyNumberFinding createCopyNumberFinding(Query query) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see gov.nih.nci.caintegrator.service.findings.FindingsFactory#createClinicalFinding(gov.nih.nci.caintegrator.dto.query.Query)
+	 */
+	public ClinicalFinding createClinicalFinding(Query query) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see gov.nih.nci.caintegrator.service.findings.FindingsFactory#createClassComparisonFinding(gov.nih.nci.caintegrator.dto.query.Query)
+	 */
+	public ClassComparisonFindingsResultset createClassComparisonFinding(gov.nih.nci.caintegrator.dto.query.ClassComparisonQuery query, String sessionID, String taskID) {
+		try {
+			ClassComparisonFindingStrategy classComparisonFindingStrategy = new  ClassComparisonFindingStrategy(sessionID,taskID, (ClassComparisonQuery) query);
+			classComparisonFindingStrategy.createQuery();
+			classComparisonFindingStrategy.executeQuery();
+			classComparisonFindingStrategy.analyzeResultSet();
+			return (ClassComparisonFindingsResultset) classComparisonFindingStrategy.getResultSet();
+			
+		} catch (ValidationException e) {
+			logger.error(e);
+		} catch (FindingsQueryException e) {
+			logger.error(e);
+		} catch (FindingsAnalysisException e) {
+			logger.error(e);
+		}
+		
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see gov.nih.nci.caintegrator.service.findings.FindingsFactory#createPCAFinding(gov.nih.nci.caintegrator.dto.query.Query)
+	 */
+	public PCAFinding createPCAFinding(Query query) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see gov.nih.nci.caintegrator.service.findings.FindingsFactory#createHCAFinding(gov.nih.nci.caintegrator.dto.query.Query)
+	 */
+	public HCAFinding createHCAFinding(Query query) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see gov.nih.nci.caintegrator.service.findings.FindingsFactory#createGEIntensityFinding(gov.nih.nci.caintegrator.dto.query.Query)
+	 */
+	public GEIntensityFinding createGEIntensityFinding(Query query) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see gov.nih.nci.caintegrator.service.findings.FindingsFactory#createCustomFinding(gov.nih.nci.caintegrator.dto.query.Query)
+	 */
+	public Object createCustomFinding(Query query) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
