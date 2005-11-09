c622afcb78ace5c3ae9f3e189f98603e80525d7b
Ryan Landy <landyr>
2005-11-09 19:31:56 +0000

    ajax related functions
    SVN-Revision: 1770

--- WebRoot/js/a_functions.js
+++ WebRoot/js/a_functions.js
@@ -1,18 +1,30 @@
-
-function A_checkFindingStatus(taskId, sessionId)	{
-
-	Inbox.checkStatus(taskId, sessionId, A_checkFindingStatus_cb);
+var vr_count = 0;
+var vr_totalRuns = 0;
+				
+function A_checkAllFindingStatus(sessionId)	{
+	showLoading(true);
+	vr_totalRuns++;
+	Inbox.checkAllStatus(sessionId, A_checkFindingStatus_cb);
+	if(vr_totalRuns > 3) 	{ //only run 3 times to avoid infinte loops
+		clearInterval(vr_checker);
+	}
 }
+
+/*
+function A_checkSingleFindingStatus(taskId, sessionId)	{
+	Inbox.checkSingleStatus(sessionId, taskId, A_checkFindingStatus_cb);
+}
+*/
 
 function A_checkFindingStatus_cb(tasks)	{
 	//looking for an assoc array arr[taskId]=status
-	for(var i=0; i<tasks.length; i++)	{
-		if(tasks[i] == 'completed')	{
+	for(key in tasks)	{
+		if(tasks[key] == 'completed')	{
 			//its done, see if the innerhtml already says done
-			var curEl = document.getElementById(tasks[i]+"_status");
-			var curElImg = document.getElementById(tasks[i]+"_image");
-			if(curEl.innerHtml != "completed")	{
-				curEl.innerHtml = "completed";
+			var curEl = document.getElementById(key+"_status");
+			var curElImg = document.getElementById(key+"_image");
+			if(curEl.innerHTML != "completed")	{
+				curEl.innerHTML = "completed";
 				curElImg.src = "images/check.png";
 			}
 		}
@@ -22,6 +34,17 @@ function A_checkFindingStatus_cb(tasks)	{
 	}
 
 }
+
+function showLoading(showit)	{
+	var msg = document.getElementById("loadingMsg");
+	if(showit)	{
+		msg.innerHTML = " <strong>Checking status...</strong>";
+		setTimeout("showLoading(false)", 1000);
+	}
+	else 
+		msg.innerHTML = "&nbsp;";
+}
+
 
 function testMap(k)	{
 	Inbox.mapTest(k, testMap_cb);
