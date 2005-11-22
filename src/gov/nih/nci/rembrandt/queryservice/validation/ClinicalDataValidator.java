fbeeb052bd0b98d4b1fffeaea08c196135e2bd32
Harris Michael <harrismic>
2005-11-22 16:39:48 +0000

    Class to check if samples have the required clinical data for the clinical plots.
    SVN-Revision: 1851

---
+++ src/gov/nih/nci/rembrandt/queryservice/validation/ClinicalDataValidator.java
@@ -0,0 +1,17 @@
+package gov.nih.nci.rembrandt.queryservice.validation;
+
+import gov.nih.nci.caintegrator.dto.de.SampleIDDE;
+import gov.nih.nci.caintegrator.enumeration.ClinicalFactorType;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class ClinicalDataValidator {
+
+
+	public static Collection<SampleIDDE> getValidatedSamples(Collection<SampleIDDE> sampleList, Collection<ClinicalFactorType> clinicalFactors) {
+	  //TODO implement this method
+	  return Collections.EMPTY_LIST;
+	}
+
+}
