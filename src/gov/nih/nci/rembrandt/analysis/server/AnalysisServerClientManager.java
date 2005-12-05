271b88992d5239aae73d53c33d06d0b08a7683fc
Himanso Sahni <Himanso.Sahni@fda.hhs.gov>
2005-12-05 22:12:05 +0000

    added error comment to findingstatus exeception
    SVN-Revision: 1967

--- src/gov/nih/nci/rembrandt/analysis/server/AnalysisServerClientManager.java
+++ src/gov/nih/nci/rembrandt/analysis/server/AnalysisServerClientManager.java
@@ -153,7 +153,7 @@ public class AnalysisServerClientManager implements MessageListener, ExceptionLi
 		AnalysisFinding finding = (AnalysisFinding)_cacheManager.getSessionFinding(sessionId, taskId);
 		if(finding != null){
 			FindingStatus newStatus = FindingStatus.Error;
-			newStatus.setComment(analysisServerException.getMessage())
+			newStatus.setComment(analysisServerException.getMessage());
 			finding.setStatus(newStatus);
 			logger.debug("Retreiving finding for session: "+sessionId+" & task: "+taskId+" from cache");
 			_cacheManager.addToSessionCache(sessionId,taskId+"_analysisServerException",analysisServerException);
