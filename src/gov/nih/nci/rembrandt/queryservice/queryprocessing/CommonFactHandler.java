4f9ebc2ed2b1a5fca16733784edef31d31521aec
Himanso Sahni <Himanso.Sahni@fda.hhs.gov>
2005-12-16 16:29:56 +0000

    added fix for DiseaseView
    SVN-Revision: 2099

--- src/gov/nih/nci/rembrandt/queryservice/queryprocessing/CommonFactHandler.java
+++ src/gov/nih/nci/rembrandt/queryservice/queryprocessing/CommonFactHandler.java
@@ -6,7 +6,14 @@ import gov.nih.nci.caintegrator.dto.critieria.InstitutionCriteria;
 import gov.nih.nci.caintegrator.dto.de.DiseaseNameDE;
 import gov.nih.nci.caintegrator.dto.de.SampleIDDE;
 import gov.nih.nci.caintegrator.dto.de.InstitutionDE;
+import gov.nih.nci.caintegrator.dto.view.ClinicalSampleView;
+import gov.nih.nci.caintegrator.dto.view.CopyNumberSampleView;
+import gov.nih.nci.caintegrator.dto.view.GeneExprDiseaseView;
+import gov.nih.nci.caintegrator.dto.view.GeneExprSampleView;
+import gov.nih.nci.caintegrator.util.CaIntegratorConstants;
+import gov.nih.nci.rembrandt.dto.query.GeneExpressionQuery;
 import gov.nih.nci.rembrandt.dto.query.Query;
+import gov.nih.nci.rembrandt.queryservice.queryprocessing.ge.GEFactHandler;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -56,7 +63,9 @@ public class CommonFactHandler {
 
     public static void addAccessCriteria(Query query, Class beanClass, Criteria criteria)
     throws Exception {
-        InstitutionCriteria institCrit = query.getInstitutionCriteria ();
+    	//This is a work around interceptor to provide proper access for GE Disease Queries
+    	
+        InstitutionCriteria institCrit = interceptorForGEDiseaseGroupQuery(query);
         if (institCrit  != null) {
             ArrayList<Long> instIDs = new ArrayList<Long>();
             Collection<InstitutionDE> institutes = institCrit.getInstitutions();
@@ -71,5 +80,57 @@ public class CommonFactHandler {
             criteria.addAndCriteria(c);
         }
     }
+	private static InstitutionCriteria  interceptorForGEDiseaseGroupQuery(Query query) {
+		// sahnih
+		//This methods allows proper selection of access Codes for GE Group Queries
+//		IF AFTER LOGIN ID = 8 ONLY THEN ID = 8 ONLY
+//		IF ID = 8 + ANY ONE INSTITUTION_ID THEN THEN ID = THAT ONE INSTITUTION_ID
+//		IF ID = 8 + 1 + 5 THEN ID = 5
+//		IF ID = 8 + ALL OTHER INSTITUTION_IDS THEN ID = 13
+		InstitutionCriteria institCrit = query.getInstitutionCriteria ();  
+		InstitutionCriteria newInstitCriteria = new InstitutionCriteria();
+		Collection<InstitutionDE> updatesInstitutes = new ArrayList<InstitutionDE>();
+		if( 	query instanceof GeneExpressionQuery  &&
+				query.getAssociatedView() instanceof GeneExprDiseaseView){
+			if (institCrit  != null) {
+		            Collection<InstitutionDE> institutes = institCrit.getInstitutions();		            
+		            if(institutes!= null ){
+		            	//	change to public
+		            	if(institutes.size()== 1 ){
+		            		for (InstitutionDE institution : institutes) {
+			 		               if(!(institution.getValueObject().equals(CaIntegratorConstants.PUBLIC_ACCESS))){
+				            			InstitutionDE institutionDE = new InstitutionDE("PUBLIC",CaIntegratorConstants.PUBLIC_ACCESS);
+					 		            updatesInstitutes.add(institutionDE);
+			 		               		}
+			            		}
+			 		    }
+		            	// remove public
+		            	else if(institutes.size() == 2){
+		            		for (InstitutionDE institution : institutes) {
+		 		               if(!(institution.getValueObject().equals(CaIntegratorConstants.PUBLIC_ACCESS))){
+		 		            	  updatesInstitutes.add(institution);
+		 		               		}
+		            		}
+		            	}
+		            	// remove public + Henry Ford
+		            	else if(institutes.size() == 3){
+		            		InstitutionDE institutionDE = new InstitutionDE("NABTT/HENRY FORD",new Long(5));
+		 		            updatesInstitutes.add(institutionDE);
+		            	}
+		            	//change to super
+		            	else if(institutes.size() > 3){
+		            		InstitutionDE institutionDE = new InstitutionDE("ALL INSTITITIONS",new Long(13));
+			 		        updatesInstitutes.add(institutionDE);
+			 		    }
+		            	
+		            }
+		        }
+		}
+		if(updatesInstitutes.size()>0){
+			newInstitCriteria.setInstitutions(updatesInstitutes);	
+			return newInstitCriteria;    		
+    	}
+			return institCrit;
+	}
 
 }
