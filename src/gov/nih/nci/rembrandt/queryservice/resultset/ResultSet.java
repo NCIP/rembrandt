b1e8366be5827f5b4a34fc195183d52b33a1a1f1
no_author <no_author@no_author.cvs2svn>
2005-12-23 19:33:21 +0000

    This commit was manufactured by cvs2svn to create tag 'rembrandt_version_0_5_3_deploy-stage_build_55'.
    SVN-Revision: 2154

--- src/gov/nih/nci/rembrandt/queryservice/resultset/ResultSet.java
+++
@@ -1,7 +0,0 @@
-package gov.nih.nci.rembrandt.queryservice.resultset;
-
-import gov.nih.nci.caintegrator.dto.query.ResultSetInterface;
-
-public interface ResultSet extends ResultSetInterface{
-    // it is simply a marker interface for now
-}
