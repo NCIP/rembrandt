3ffd85d6fec6da201f42755b9d44d25bf73e6bb3
Ram Bhattaru <bhattarr>
2006-02-02 17:00:23 +0000

    no message
    SVN-Revision: 2243

---
+++ src/gov/nih/nci/rembrandt/queryservice/queryprocessing/ge/annotations/ReporterAnnotationsHandler.java
@@ -0,0 +1,80 @@
+package gov.nih.nci.rembrandt.queryservice.queryprocessing.ge.annotations;
+
+import org.apache.ojb.broker.query.Criteria;
+import org.apache.ojb.broker.query.Query;
+import org.apache.ojb.broker.query.QueryFactory;
+import org.apache.ojb.broker.PersistenceBroker;
+import org.apache.ojb.broker.PersistenceBrokerFactory;
+import org.apache.log4j.Logger;
+
+import java.util.*;
+
+import gov.nih.nci.rembrandt.dbbean.ProbesetDim;
+import gov.nih.nci.rembrandt.queryservice.queryprocessing.DBEvent;
+import gov.nih.nci.rembrandt.queryservice.queryprocessing.QueryHandler;
+import gov.nih.nci.rembrandt.util.ThreadPool;
+
+/**
+ * User: Ram Bhattaru <BR>
+ * Date: Feb 1, 2006 <BR>
+ * Version: 1.0 <BR>
+ */
+public class ReporterAnnotationsHandler {
+
+    private static Logger logger = Logger.getLogger(AnnotationsHelper.class);
+    private final static int VALUES_PER_THREAD = 50;
+
+
+    public  Map<String,ReporterDimension>  execAnnotationQuery(List probeNames, List annotationsEventList) throws Exception {
+            final Map<String, ReporterDimension> probeAnnotations = Collections.synchronizedMap(new HashMap());
+            for (int i = 0; i < probeNames.size();) {
+                Collection values = new ArrayList();
+                int begIndex = i;
+                i += VALUES_PER_THREAD ;
+                int endIndex = (i < probeNames.size()) ? endIndex = i : (probeNames.size());
+                values.addAll(probeNames.subList(begIndex,  endIndex));
+                final Criteria annotCrit = new Criteria();
+                annotCrit.addIn(ProbesetDim.PROBESET_NAME, values);
+                long time = System.currentTimeMillis();
+                String threadID = "AnnotationHelper.ThreadID:" +time;
+                final DBEvent.AnnotationRetrieveEvent dbEvent = new DBEvent.AnnotationRetrieveEvent(threadID);
+                annotationsEventList.add(dbEvent);
+                final PersistenceBroker _BROKER = PersistenceBrokerFactory.defaultPersistenceBroker();
+                _BROKER.clearCache();
+                final String locusLinkColName = QueryHandler.getColumnNameForBean(_BROKER, ProbesetDim.class.getName(), ProbesetDim.LOCUS_LINK);
+                final String accessionColName = QueryHandler.getColumnNameForBean(_BROKER, ProbesetDim.class.getName(), ProbesetDim.ACCESSION_NUMBER);
+                final String probeIDColName = QueryHandler.getColumnNameForBean(_BROKER, ProbesetDim.class.getName(), ProbesetDim.PROBESET_NAME);
+                _BROKER.close();
+                ThreadPool.AppThread t = ThreadPool.newAppThread(
+                       new ThreadPool.MyRunnable() {
+                          public void codeToRun()  {
+                              final PersistenceBroker pb = PersistenceBrokerFactory.defaultPersistenceBroker();
+                              pb.clearCache();
+                              Query annotQuery =
+                              QueryFactory.newReportQuery(ProbesetDim.class,new String[] {probeIDColName , locusLinkColName, accessionColName }, annotCrit, false);
+                              assert(annotQuery != null);
+                              Iterator iter =  pb.getReportQueryIteratorByQuery(annotQuery);
+                              while (iter.hasNext()) {
+                                   Object[] probeAttrs = (Object[]) iter.next();
+                                   String probeName = (String)probeAttrs[0];
+                                   ReporterDimension r = (ReporterDimension)probeAnnotations.get(probeName);
+                                   if (r == null) {
+                                       r = new ReporterDimension(new ArrayList<String>(), new ArrayList<String>(), probeName);
+                                       probeAnnotations.put(probeName , r);
+                                   }
+                                   r.getLocusLinks().add((String)probeAttrs[1]);
+                                   r.getAccessions().add((String)probeAttrs[2]);
+                              }
+                              pb.close();
+                              dbEvent.setCompleted(true);
+                      }
+                   }
+               );
+               logger.debug("BEGIN (from AnnotationsHelper.executeProbeAnnotationQuery()): Thread Count: " + ThreadPool.THREAD_COUNT);
+               t.start();
+            }
+
+         return probeAnnotations;
+     }
+
+}
---
+++ src/gov/nih/nci/rembrandt/queryservice/test/AnnotationsHelperTest.java
@@ -0,0 +1,128 @@
+package gov.nih.nci.rembrandt.queryservice.test;
+
+import junit.framework.TestCase;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import gov.nih.nci.rembrandt.queryservice.queryprocessing.ge.annotations.AnnotationsHelper;
+import gov.nih.nci.rembrandt.queryservice.queryprocessing.ge.annotations.ReporterAnnotations;
+import gov.nih.nci.rembrandt.util.ApplicationContext;
+import gov.nih.nci.rembrandt.dbbean.ProbesetDim;
+
+import java.util.*;
+
+import org.apache.ojb.broker.PersistenceBrokerFactory;
+import org.apache.ojb.broker.PersistenceBroker;
+import org.apache.ojb.broker.query.QueryFactory;
+import org.apache.ojb.broker.query.Query;
+
+/**
+ * User: Ram Bhattaru <BR>
+ * Date: Feb 1, 2006 <BR>
+ * Version: 1.0 <BR>
+ */
+public class AnnotationsHelperTest extends TestCase {
+    List reporters;
+    protected void setUp() throws Exception {
+        ApplicationContext.init();
+
+        /* populate reporters for testing testgetGeneSymbolsForReportersQuery() */
+        final PersistenceBroker pb = PersistenceBrokerFactory.defaultPersistenceBroker();
+        pb.clearCache();
+        Query q = QueryFactory.newReportQuery(ProbesetDim.class,new String[] {ProbesetDim.PROBESET_NAME}, null, true);
+        q.setStartAtIndex(0);
+        q.setEndAtIndex(8000);
+        reporters = new ArrayList();
+        Iterator iter =  pb.getReportQueryIteratorByQuery(q);
+        while (iter.hasNext()) {
+           Object[] geneAttrs = (Object[]) iter.next();
+           reporters.add((String)geneAttrs[0]);
+        }
+    }
+
+    public void testgetGeneSymbolsFor() throws Exception{
+        AnnotationsHelper h = new AnnotationsHelper();
+        Map results = h.getGeneSymbolsFor(reporters);
+
+       // loop through and print all reporters and associated gene symbols
+        for (Iterator iterator = reporters.iterator(); iterator.hasNext();) {
+            String r =  (String) iterator.next();
+            System.out.println("REPORTER:" + r + "    GENE_SYMBOL:" + results.get(r));
+        }
+
+    }
+
+     public void testgetAllAnnotationsFor() throws Exception{
+         AnnotationsHelper h = new AnnotationsHelper();
+         Map<String, ReporterAnnotations> results = h.getAllAnnotationsFor(reporters);
+
+        // loop through and print all reporters and associated gene symbols
+        for (Iterator iterator = reporters.iterator(); iterator.hasNext();) {
+            String r =  (String) iterator.next();
+            ReporterAnnotations ra = results.get(r);
+
+            // NOTE: any of the below annotations could be null.  So please check for null
+            // before accessing these annotations (such as accns, locuslinks, goID, pathways
+            ArrayList accns = ra.getAccessions();
+            ArrayList locusLinks = ra.getLocusLinks();
+            ArrayList goIDs = ra.getGoIDS();
+            ArrayList pathways = ra.getPathways();
+
+
+            System.out.print("REPORTER:" + r);
+            System.out.print("  GENE_SYMBOL:" + ra.getGeneSymbol());
+
+            System.out.println("\n***** BEGIN ACCESSIONS: *************");
+            if (accns != null) {
+                for (Iterator iterator1 = accns.iterator(); iterator1.hasNext();) {
+                    Object o =  iterator1.next();
+                    System.out.println(" " + o);
+
+                }
+            }
+            System.out.println("***** END ACCESSIONS: *************\n");
+
+
+            System.out.println("***** BEGIN LOCUSLINKS: *************");
+            if (locusLinks != null) {
+                for (Iterator iterator1 = locusLinks.iterator(); iterator1.hasNext();) {
+                    Object o =  iterator1.next();
+                    System.out.println(" " + o);
+
+                }
+            }
+            System.out.println("***** END LOCUSLINKS: *************\n");
+
+            System.out.println("***** BEGIN GOIDS: *************");
+            if (goIDs != null) {
+                for (Iterator iterator1 = goIDs.iterator(); iterator1.hasNext();) {
+                    Object o =  iterator1.next();
+                    System.out.println(" " + o);
+
+                }
+            }
+            System.out.println("***** END GOIDS: *************\n");
+
+            System.out.println("***** BEGIN PATHWAYS: *************");
+            if (pathways != null) {
+                for (Iterator iterator1 = pathways.iterator(); iterator1.hasNext();) {
+                    Object o =  iterator1.next();
+                    System.out.println(" " + o);
+
+                }
+            }
+            System.out.println("***** END PATHWAYS: *************\n");
+        }
+     }
+
+    public static Test suite() {
+		TestSuite suit =  new TestSuite();
+        suit.addTest(new TestSuite(AnnotationsHelperTest.class));
+        return suit;
+	}
+
+	public static void main (String[] args) {
+		junit.textui.TestRunner.run(suite());
+
+    }
+
+}
