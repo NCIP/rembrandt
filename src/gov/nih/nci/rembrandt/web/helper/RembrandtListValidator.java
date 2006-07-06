4d4fc0c5f6b8e52bc69514c8e8687645d1d63e5d
Kevin Rosso <Kevin.Rosso@fda.hhs.gov>
2006-07-06 16:25:39 +0000

    *** empty log message ***
    SVN-Revision: 2311

---
+++ src/gov/nih/nci/rembrandt/web/helper/RembrandtListValidator.java
@@ -0,0 +1,20 @@
+package gov.nih.nci.rembrandt.web.helper;
+
+import java.util.List;
+
+import gov.nih.nci.caintegrator.application.lists.ListType;
+import gov.nih.nci.caintegrator.application.lists.ListValidator;
+
+public class RembrandtListValidator implements ListValidator{
+
+    public List getValidList(ListType listType, List<String> unvalidatedList) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public List getInvalidList(ListType listType, List<String> unvalidatedList) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
