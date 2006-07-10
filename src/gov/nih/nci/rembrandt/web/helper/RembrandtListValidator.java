ab7e8267943148686938131e780647a9883a1e12
Himanso Sahni <Himanso.Sahni@fda.hhs.gov>
2006-07-10 22:44:15 +0000

    added impl methods
    SVN-Revision: 2322

--- src/gov/nih/nci/rembrandt/web/helper/RembrandtListValidator.java
+++ src/gov/nih/nci/rembrandt/web/helper/RembrandtListValidator.java
@@ -2,18 +2,31 @@ package gov.nih.nci.rembrandt.web.helper;
 
 import java.util.List;
 
+import gov.nih.nci.caintegrator.application.lists.ListSubType;
 import gov.nih.nci.caintegrator.application.lists.ListType;
 import gov.nih.nci.caintegrator.application.lists.ListValidator;
 
 public class RembrandtListValidator implements ListValidator{
+	private ListType listType;
+	private ListSubType listSubType;
+	private List<String> unvalidatedList;
+	private List<String> validatedList;
+	public RembrandtListValidator(ListSubType listSubType, ListType listType, List<String> unvalidatedList) {
+		super();
+		this.listSubType = listSubType;
+		this.listType = listType;
+		this.unvalidatedList = unvalidatedList;
+	}
 
     public List getValidList(ListType listType, List<String> unvalidatedList) {
-        // TODO Auto-generated method stub
+		this.listType = listType;
+		this.unvalidatedList = unvalidatedList;
         return unvalidatedList;
     }
 
     public List getInvalidList(ListType listType, List<String> unvalidatedList) {
-        // TODO Auto-generated method stub
+		this.listType = listType;
+		this.unvalidatedList = unvalidatedList;
         return unvalidatedList;
     }
 
