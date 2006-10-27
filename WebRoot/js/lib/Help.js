65ec64b07346abd7f72081817f077a8af4e97395
Ryan Landy <landyr>
2006-10-27 18:48:36 +0000

    removed the "always on top" bit
    SVN-Revision: 2613

--- WebRoot/js/lib/Help.js
+++ WebRoot/js/lib/Help.js
@@ -1,7 +1,9 @@
 var Help = {
 	url : "helpDocs/REMBRANDT_Online_Help/index.html?single=true&context=REMBRANDT_Online_Help&topic=",
 	popHelp: function(topic) {
-		window.open (Help.url+topic, "Help", "alwaysRaised,dependent,status,scrollbars,resizable,width=800,height=500");  
+		window.open (Help.url+topic, "Help", "status,scrollbars,resizable,width=800,height=500");  
+		//use the below if you want the "always on top" feature, most dont like it
+		//window.open (Help.url+topic, "Help", "alwaysRaised,dependent,status,scrollbars,resizable,width=800,height=500");  
 		//alert(Help.url+topic);
 	},
 	insertHelp: function(topic)	{
@@ -14,6 +16,8 @@ var Help = {
 	popHelpMain: function(topic) {
 		//var _url = "helpDocs/REMBRANDT_Online_Help/index.html?single=false&context=REMBRANDT_Online_Help&topic=";
 		var _url = "helpDocs/REMBRANDT_Online_Help/WelcometoREMBRANDTOnlineHelp.1.1.html";
+		window.open (_url+topic, "Help", "status,scrollbars,resizable,width=800,height=500");  
+		//use the below if you want the "always on top" feature, most dont like it		
 		window.open (_url+topic, "Help", "alwaysRaised,dependent,status,scrollbars,resizable,width=800,height=500");  
 		//alert(Help.url+topic);
 	},
