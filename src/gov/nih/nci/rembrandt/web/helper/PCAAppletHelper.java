992a991170ddbec3716f0a963f4a22f1d4cf7b15
Ryan Landy <landyr>
2006-11-01 23:51:48 +0000

    grouped by disease
    SVN-Revision: 2651

--- src/gov/nih/nci/rembrandt/web/helper/PCAAppletHelper.java
+++ src/gov/nih/nci/rembrandt/web/helper/PCAAppletHelper.java
@@ -1,5 +1,6 @@
 package gov.nih.nci.rembrandt.web.helper;
 
+import java.awt.Color;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.ArrayList;
@@ -29,7 +30,8 @@ public class PCAAppletHelper {
 
 	public static String generateParams(String sessionId, String taskId){
 		String htm = "";
-		//DecimalFormat nf = new DecimalFormat("0.0000")
+		DecimalFormat nf = new DecimalFormat("0.0000");
+		
 		try {
             //retrieve the Finding from cache and build the list of PCAData points
             PrincipalComponentAnalysisFinding principalComponentAnalysisFinding = (PrincipalComponentAnalysisFinding)businessTierCache.getSessionFinding(sessionId, taskId);
@@ -84,27 +86,85 @@ public class PCAAppletHelper {
                 }
             }
             
+            //make a hashmap
+            // [key=group] hold the array of double[][]s
+            HashMap<String, ArrayList> hm = new HashMap();
+            
             //now we should have a collection of PCADataPts
             double[][] pts = new double[pcaData.size()][3];
             for(int i=0; i<pcaData.size();i++)	{
             	//just create a large 1 set for now
             	//are we breaking groups by gender or disease?
             	PrincipalComponentAnalysisDataPoint pd = pcaData.get(i);
+            	
         		pts[i][0] = pd.getPc1value();
         		pts[i][1] = pd.getPc2value();
         		pts[i][2] = pd.getPc3value();
+        		ArrayList<double[]> al;
+        		
+        		try	{
+	        		if(hm.containsKey(pd.getDiseaseName())){
+	            		//already has it, so add this one
+	            		al = (ArrayList) hm.get(pd.getDiseaseName());
+	            	}
+	            	else	{
+	            		al = new ArrayList();
+	            		hm.put(pd.getDiseaseName(), new ArrayList());
+	            	}
+	            	if(!al.contains(pts[i]))	{
+	            		al.add(pts[i]);
+	            	}
+	            	hm.put(pd.getDiseaseName(), al);
+        		}
+        		catch(Exception e){
+        			System.out.print(e.toString());
+        		}
             }
+            int r = hm.size();
+            if(r==1){}
+            //hm should now contain a hashmap of all the disease groups
             
             //generate the param tags
+            htm += "<param name=\"key\" value=\""+taskId+"\" >\n";
             htm += "<param name=\"totalPts\" value=\""+pts.length+"\" >\n";
+            htm += "<param name=\"totalGps\" value=\""+hm.size()+"\" >\n";
+            int ii = 0;
+            for(Object k : hm.keySet())	{
+            	String key = k.toString();
+            	//for each group
+            	
+            	Color diseaseColor = Color.GRAY;
+            	if(DiseaseType.valueOf(key)!=null)	{
+            		DiseaseType disease = DiseaseType.valueOf(key);
+            		diseaseColor = disease.getColor();
+            	}
+      		  
+            	ArrayList<double[]> al = hm.get(key);
+            	htm+= "<param name=\"groupLabel_"+ii+"\" value=\""+key+"\" >\n";
+            	htm+= "<param name=\"groupCount_"+ii+"\" value=\""+al.size()+"\" >\n";
+            	htm+= "<param name=\"groupColor_"+ii+"\" value=\""+diseaseColor.getRGB()+"\" >\n";
+            	int jj=0;
+            	for(double[] d : al){
+            		String comm = nf.format(d[0]) + "," +
+                	nf.format(d[1]) + "," +
+                	nf.format(d[2]);
+            		String h = "<param name=\"pt_"+ii+"_"+jj+"\" value=\""+ comm +"\">\n";
+            		htm+=h;
+            		jj++;
+            	}
+            	ii++;
+            }
+            /*
+             //for bulk rendering
             for(int i=0; i<pts.length; i++)	{
             	String comm = String.valueOf(pts[i][0]) + "," +
             	String.valueOf(pts[i][1]) + "," +
             	String.valueOf(pts[i][2]);
             	
             	String h = "<param name=\"pt_"+i+"\" value=\""+ comm +"\">\n";
-            	htm += h;
+            	//htm += h;
             }
+            */
             
 		}//try
 		catch(Exception e){
