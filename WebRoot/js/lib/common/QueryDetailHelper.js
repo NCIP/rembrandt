d7f72abeee7290843dc67c4f4b0dae3e5765ebfa
Ryan Landy <landyr>
2006-11-29 15:23:22 +0000

    wrong param order?
    SVN-Revision: 2745

--- WebRoot/js/lib/common/QueryDetailHelper.js
+++ WebRoot/js/lib/common/QueryDetailHelper.js
@@ -2,7 +2,7 @@
 var QueryDetailHelper = {
 	'delay' : 250,
 	'getQueryDetails' : function(queryName)	{
-		Inbox.getQueryDetailFromCompoundQuery(QueryDetailHelper.getQueryDetails_cb, queryName);
+		Inbox.getQueryDetailFromCompoundQuery(queryName, QueryDetailHelper.getQueryDetails_cb);
 	},
 	'getQueryDetails_cb' : function(txt){
 		return overlib(txt, CAPTION,  "Query");
