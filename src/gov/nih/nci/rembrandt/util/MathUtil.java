11ef25c1b290eed51e4406a479668f39bd53d92c
Himanso Sahni <Himanso.Sahni@fda.hhs.gov>
2006-12-01 17:38:54 +0000

    aded antilog2
    SVN-Revision: 2776

--- src/gov/nih/nci/rembrandt/util/MathUtil.java
+++ src/gov/nih/nci/rembrandt/util/MathUtil.java
@@ -5,18 +5,30 @@ package gov.nih.nci.rembrandt.util;
 
 /**
  * @author sahnih
- *
+ * 
  */
 
 public class MathUtil {
 	/**
 	 * The method returns you the log2 value
-	 *
-	 */	
-public static Double getLog2(Double value){
-	if(value != null){
-		return Math.log(value)/Math.log(2);
+	 * 
+	 */
+	public static Double getLog2(Double value) {
+		if (value != null) {
+			return Math.log(value) / Math.log(2);
+		}
+		return null;
 	}
-	return null;
-}
+
+	/**
+	 * The method returns you the antilogarithm of log base 2
+	 * 
+	 */
+	public static Double getAntiLog2(Double value) {
+		// Calculate the antilogarithm of log base 2
+		if (value != null) {
+			return Math.pow(2.0, value);
+		}
+		return null;
+	}
 }
