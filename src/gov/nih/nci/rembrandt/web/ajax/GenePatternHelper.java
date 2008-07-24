5f392c2cb297d570beafba8e711a630fee3214ad
Hangjiong Chen <chenhang>
2008-07-24 17:54:47 +0000

    AnalysisWebServiceProxy instance is saved in session.
    SVN-Revision: 3255

--- src/gov/nih/nci/rembrandt/web/ajax/GenePatternHelper.java
+++ src/gov/nih/nci/rembrandt/web/ajax/GenePatternHelper.java
@@ -3,20 +3,15 @@ package gov.nih.nci.rembrandt.web.ajax;
 import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
-import org.genepattern.client.GPServer;
 import org.genepattern.webservice.AnalysisWebServiceProxy;
 import org.genepattern.webservice.JobInfo;
-import org.genepattern.webservice.TaskIntegratorProxy;
 import org.genepattern.webservice.WebServiceException;
 
 import uk.ltd.getahead.dwr.ExecutionContext;
-import gov.nih.nci.caintegrator.application.cache.CacheFactory;
-import gov.nih.nci.caintegrator.application.cache.PresentationTierCache;
 import gov.nih.nci.caintegrator.service.task.GPTask;
 import gov.nih.nci.caintegrator.enumeration.FindingStatus;
 import gov.nih.nci.rembrandt.cache.RembrandtPresentationTierCache;
 import gov.nih.nci.rembrandt.web.factory.ApplicationFactory;
-import gov.nih.nci.rembrandt.web.struts.action.GPProcessAction;
 
 import java.io.Serializable;
 
@@ -26,19 +21,28 @@ public class GenePatternHelper {
     } 
 
 	public String checkGPStatus(String sid) {
-		//System.out.println("checkGPStatus called...");
 		HttpSession session = ExecutionContext.get().getSession();
-		String gpserverURL = System.getProperty("gov.nih.nci.caintegrator.gp.server");
-		String gpUserId = (String)session.getAttribute("gpUserId");
-		String password = System.getProperty("gov.nih.nci.caintegrator.gp.publicuser.password");
+
 		int jobNumber = Integer.parseInt(sid);
-        AnalysisWebServiceProxy analysisProxy = null;
 		String message  = null;
         JobInfo info = null;
         FindingStatus findingStatus = FindingStatus.Running;
-		try {
-			analysisProxy = new AnalysisWebServiceProxy(gpserverURL, gpUserId, password, false);
+        
+    	AnalysisWebServiceProxy analysisProxy = (AnalysisWebServiceProxy)session.getAttribute("AnalysisWebServiceProxy");
+    	if (analysisProxy == null){
+    		String gpserverURL = System.getProperty("gov.nih.nci.caintegrator.gp.server");
+    		String password = System.getProperty("gov.nih.nci.caintegrator.gp.publicuser.password");
+    		String gpUserId = (String)session.getAttribute("gpUserId");
 		
+    		try{
+    			analysisProxy = new AnalysisWebServiceProxy(gpserverURL, gpUserId, password, false);
+    			session.setAttribute("AnalysisWebServiceProxy", analysisProxy);
+    		}catch (WebServiceException we){
+    			logger.error("Failed to obtain AnalysisWebServiceProxy: " + we.getMessage());
+    		}
+    	}
+		try {
+
 			if(analysisProxy != null){
 				analysisProxy.setTimeout(Integer.MAX_VALUE);
 				info = analysisProxy.checkStatus(jobNumber);
