4173a754c11508d4b347153fb664faf43aa3cfa7
Hangjiong Chen <chenhang>
2008-11-21 17:17:06 +0000

    Code change due to change in other code.
    SVN-Revision: 3321

--- src/gov/nih/nci/rembrandt/web/struts/form/UIFormValidator.java
+++ src/gov/nih/nci/rembrandt/web/struts/form/UIFormValidator.java
@@ -144,7 +144,7 @@ public class UIFormValidator {
 			errors.add("queryName", new ActionError(
 					"gov.nih.nci.nautilus.ui.struts.form.queryname.no.error"));
 		}
-		if (!MoreStringUtils.isHTMLSafe(queryName))
+		if (!MoreStringUtils.isURLSafe(queryName))
 			errors.add("queryName", new ActionError(
 				"gov.nih.nci.nautilus.ui.struts.form.queryname.illegal.characters"));
 		return errors;
